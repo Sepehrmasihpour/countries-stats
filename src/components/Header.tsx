d51bbfbd4e92d9358c10aade31fae347c836a6d9
sepehr masihpour <sepehr.masihpour1381@gmail.com>
2023-10-13 15:33:23 +0330

    started the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,15 @@
-function Header(props) {
-  return <header></header>;
+interface headerProps {
+  darkModeToggle: () => void;
+}
+
+function Header(props: headerProps) {
+  return (
+    <header>
+      <div className="header-container">
+        <h1>where in the world</h1>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
