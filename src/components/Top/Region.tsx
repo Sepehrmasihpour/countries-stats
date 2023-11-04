12f42ec4689b2b535f596133c1f7afcae05319c4
Hepheastus <sepehr.masihpour1381@gmail.com>
2023-11-04 20:38:30 +0330

    finished the react code for the top component
    made one state on the top of the component for if thew region list must be showed or not made the function for comparing the name of the region with the chosen region to see if the cregion is the chosen region , this is for the id of the regions

--- src/components/Top/Region.tsx
+++ src/components/Top/Region.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import arrow from "./assets/chevron-down-outline.svg";
 import "./Top.scss";
 
 interface RegionProps {
@@ -7,7 +9,98 @@ interface RegionProps {
 }
 
 function Region(props: RegionProps) {
-  return <div className={[props.darkMode?"Region":"dark-Region"]}></div>;
+  const [showRegions, setShowRegions] = useState(false);
+  const [chosenRegion, setChosenRegion] = useState<string | null>(null);
+
+  const detirmineChosenRegion = (
+    chosenRegion: string | null,
+    regionClass: string
+  ) => {
+    if (!chosenRegion) {
+      return false;
+    } else {
+      if (chosenRegion === regionClass) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
+  return (
+    <div className={props.darkMode ? "Region" : "dark-Region"}>
+      <div
+        className="filter-button"
+        onClick={() => setShowRegions(!showRegions)}
+      >
+        <p>Filter by Region</p>
+        <img
+          className={showRegions ? "down-arrow" : "up-arrow"}
+          src={arrow}
+          alt="region arrow"
+        />
+      </div>
+      {showRegions && (
+        <div className="region-list">
+          <p
+            id={
+              detirmineChosenRegion(chosenRegion, "america")
+                ? "chosen-country"
+                : "normal-country"
+            }
+            className="america"
+            onClick={() => setChosenRegion("america")}
+          >
+            America
+          </p>
+          <p
+            id={
+              detirmineChosenRegion(chosenRegion, "africa")
+                ? "chosen-country"
+                : "normal-country"
+            }
+            className="africa"
+            onClick={() => setChosenRegion("africa")}
+          >
+            Africa
+          </p>
+          <p
+            id={
+              detirmineChosenRegion(chosenRegion, "asia")
+                ? "chosen-country"
+                : "normal-country"
+            }
+            className="asia"
+            onClick={() => setChosenRegion("asia")}
+          >
+            Asia
+          </p>
+          <p
+            id={
+              detirmineChosenRegion(chosenRegion, "europe")
+                ? "chosen-country"
+                : "normal-country"
+            }
+            className="europe"
+            onClick={() => setChosenRegion("europe")}
+          >
+            Europe
+          </p>
+          <p
+            id={
+              detirmineChosenRegion(chosenRegion, "ocenia")
+                ? "chosen-country"
+                : "normal-country"
+            }
+            className="ocenia"
+            onClick={() => setChosenRegion("ocenia")}
+          >
+            Ocenia
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Region;
